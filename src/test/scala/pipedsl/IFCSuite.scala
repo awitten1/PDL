eefc91d684d190210c4210f4475d21136f1f2846
Andrew Witten <awitten1@terpmail.umd.edu>
2025-12-03 21:33:47 -0500

    add more tests

--- src/test/scala/pipedsl/IFCSuite.scala
+++ src/test/scala/pipedsl/IFCSuite.scala
@@ -20,7 +20,6 @@ class IFCSuite extends AnyFunSuite {
     val canonProg = new TypeInference(false).checkProgram(canonProg2)
     val basetypes = BaseTypeChecker.check(canonProg, None)
 
-    // Run IFC check
     IFCTypeChecker.check(canonProg, Some(basetypes))
   }
 
@@ -28,13 +27,37 @@ class IFCSuite extends AnyFunSuite {
     val code = """
       pipe main()[] {
         secret<int<32>> s = 10<32>;
-        public<int<32>> p = s; // Secret to Public is FAIL
+        // this fails because secret information is not allowed to flow to public.
+        public<int<32>> p = s;
       }
       circuit {
         call main();
       }
     """
-    assertThrows[RuntimeException] { // IFCTypeChecker throws RuntimeException subclasses
+    assertThrows[RuntimeException] {
+      testIFC(code)
+    }
+  }
+
+
+  test("IFC Fail Test Implicit") {
+    val code = """
+      pipe main(num: public<int<32>>)[] {
+        secret<int<32>> s = 10<32>;
+
+        // this fails to type check.  Why?
+        // because the if(secret) elevates the security context.
+        // that means that the PC is given an elevated label.
+        // in those contexts we cannot assign to a public variable.
+        if (s > num) {
+          public<int<32>> s2 = 11<32>;
+        }
+      }
+      circuit {
+        call main(3<32>);
+      }
+    """
+    assertThrows[RuntimeException] {
       testIFC(code)
     }
   }
@@ -43,10 +66,11 @@ class IFCSuite extends AnyFunSuite {
     val code = """
       pipe main()[]: bool {
         public<int<32>> p = 20<32>;
-        secret<int<32>> s = p; // Public to Secret is OK
+        secret<int<32>> s = p;
 
-        if (p > 10<32>) { // Public condition
-            secret<int<32>> s2 = 30<32>; // Assignment to Secret in Public context is OK
+        if (p > 10<32>) {
+            // assignment to Secret in Public context is OK
+            secret<int<32>> s2 = 30<32>;
         }
       }
       circuit {
@@ -55,4 +79,25 @@ class IFCSuite extends AnyFunSuite {
     """
     testIFC(code)
   }
+
+  test("IFC Valid Implicit Flow") {
+    val code = """
+      pipe main(num: secret<int<32>>)[] {
+        secret<int<32>> s = 10<32>;
+
+        // this typechecks because secreet -> secret flows are ok.
+        if (s > num) {
+          secret<int<32>> s2 = 11<32>;
+        }
+
+        // now the security context is downgraded
+        public<int<32>> s3 = 12<32>;
+      }
+      circuit {
+        call main(3<32>);
+      }
+    """
+    testIFC(code)
+  }
+
 }
